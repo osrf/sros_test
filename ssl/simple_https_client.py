33c19ed70dc508af84abdee294c47ad22d9a8571
Morgan Quigley <morgan@osrfoundation.org>
2016-05-23 11:09:20 -0700

    client now actually authenticates the server

--- ssl/simple_https_client.py
+++ ssl/simple_https_client.py
@@ -4,12 +4,11 @@ import ssl
 import socket
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-context.check_hostname = False
 context.verify_mode = ssl.CERT_REQUIRED
+context.check_hostname = True
 context.load_verify_locations('root.cert')
 context.load_cert_chain('client.cert', keyfile='client.key')
-#proxy = xmlrpcclient.ServerProxy('https://localhost:11311', context=context)
-conn = context.wrap_socket(socket.socket(socket.AF_INET))
+conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname='master.server')
 conn.connect(('localhost',12345))
 print('connected')
 data = conn.read()
