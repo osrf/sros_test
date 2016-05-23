4217f51d4732b074649df905bfb65f0fd151e103
Morgan Quigley <morgan@osrfoundation.org>
2016-05-23 10:42:34 -0700

    say that keys can be used for clients as well as servers

--- ssl/simple_https_client.py
+++ ssl/simple_https_client.py
@@ -4,6 +4,7 @@ import ssl
 import socket
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.check_hostname = False
 context.verify_mode = ssl.CERT_REQUIRED
 context.load_verify_locations('root.cert')
 context.load_cert_chain('client.cert', keyfile='client.key')
