a1a4e18523ef2121a4ad856bcc18684cfb75b615
Morgan Quigley <morgan@osrfoundation.org>
2016-05-23 14:10:03 -0700

    bidirectional authentication

--- ssl/simple_https_client.py
+++ ssl/simple_https_client.py
@@ -3,11 +3,13 @@ from __future__ import print_function
 import ssl
 import socket
 
+prefix = '' #evil.'
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.verify_mode = ssl.CERT_REQUIRED
 context.check_hostname = True
-context.load_verify_locations('root.cert')
-context.load_cert_chain('client.cert', keyfile='client.key')
+stem = prefix + 'client'
+context.load_verify_locations(prefix+'root.cert')
+context.load_cert_chain(stem+'.cert', keyfile=(stem+'.key'))
 conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname='master.server')
 conn.connect(('localhost',12345))
 print('connected')
--- ssl/simple_https_server.py
+++ ssl/simple_https_server.py
@@ -2,20 +2,43 @@
 from __future__ import print_function
 import ssl
 import socket
+import sys
 
+def servername_callback(sock, desired_server_name, context):
+    print("client is requesting to talk to [%s]" % desired_server_name)
+    if desired_server_name != 'master.server':
+        print("get outta here")
+        return ssl.ALERT_DESCRIPTION_HANDSHAKE_FAILURE
+    return None # accept it
+
+prefix = '' #evil.'
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.verify_mode = ssl.CERT_REQUIRED
-context.load_verify_locations('root.cert')
-context.load_cert_chain('server.cert', keyfile='server.key')
+context.load_verify_locations(prefix+'root.cert')
+context.load_cert_chain(prefix+'server.cert', keyfile=(prefix+'server.key'))
+context.set_servername_callback(servername_callback)
 bindsocket = socket.socket()
+bindsocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 bindsocket.bind(('localhost',12345))
 bindsocket.listen(5)
+
 while True:
-    newsocket, fromaddr = bindsocket.accept()
-    print('accepted')
-    connstream = context.wrap_socket(newsocket, server_side=True)
-    print('wrapped')
-    connstream.send('hello')
-    connstream.shutdown(socket.SHUT_RDWR)
-    connstream.close()
-    print('closed')
+    try:
+        newsocket, fromaddr = bindsocket.accept()
+        print('accepted')
+        connstream = context.wrap_socket(newsocket, server_side=True)
+        cert = connstream.getpeercert()
+        ssl.match_hostname(cert, 'master.client')
+        print('client name matched OK')
+        print('sending secret message')
+        connstream.send('hello')
+        connstream.shutdown(socket.SHUT_RDWR)
+        connstream.close()
+        print('closed')
+    except KeyboardInterrupt:
+        print('\n\nadios amigo\n')
+        break
+    except ssl.CertificateError as e:
+        print('ssl certificate exception: %s' % e)
+bindsocket.shutdown(socket.SHUT_RDWR)
+bindsocket.close()
